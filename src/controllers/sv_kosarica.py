2403121facad6acb76db1ac64b3e689bf52a4f8b
Bertonclova <maja.bertoncelj@student.um.si>
2025-06-01 20:55:12 +0200

    spreminila vnos postnine

--- src/controllers/sv_kosarica.py
+++ src/controllers/sv_kosarica.py
@@ -6,7 +6,7 @@ def add_to_cart():
     models.sv_kosarica.dodaj_v_kosarico(product_id)
     return redirect(url_for('trgovina'))
 
-def izracunaj_postnino(drzava, regija, nacin):
+def izracunaj_postnino(drzava, nacin):
     # Base price
     base_price = 2.0
 
@@ -15,16 +15,6 @@ def izracunaj_postnino(drzava, regija, nacin):
         base_price += 5.0
     elif drzava == "slo":
         base_price += 1.0
-
-    # Region adjustment
-    region_modifiers = {
-        "osrednjeslovenska": 0.0,
-        "podravska": 0.5,
-        "gorenjska": 0.7,
-        "primorska": 1.0,
-        "dolenjska": 0.6
-    }
-    base_price += region_modifiers.get(regija, 0.0)
 
     # Delivery method adjustment
     if nacin == "obicajno":
@@ -39,13 +29,12 @@ def izracunaj_postnino(drzava, regija, nacin):
 def izpis_kosarice():
     postnina = None
     drzava = request.args.get('drzava')
-    regija = request.args.get('regija')
     nacin = request.args.get('nacin')
 
     print("request.arg", request.args, flush=True)
 
-    if drzava and regija and nacin:
-        postnina = izracunaj_postnino(drzava, regija, nacin)
+    if drzava  and nacin:
+        postnina = izracunaj_postnino(drzava, nacin)
 
     izdelki = models.sv_kosarica.izpis_kosarice()
     return render_template('sv_izpis_kosarice.html', izdelki=izdelki, postnina=postnina)
